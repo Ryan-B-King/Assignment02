7d6476b4b74dffbdab05a33cd73871b64e077faa
Rockharrd <33941531+Rockharrd@users.noreply.github.com>
2020-02-06 18:43:02 -0800

    Updated Larger or Smaller

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,14 +1,16 @@
 let num1;
 let num2;
 
-num1 = window.prompt("I'll tell you the LARGER of two numbers! \n\nTell me your first number:");
-num2 = window.prompt("I'll tell you the LARGER of two numbers! \n\nTell me your second numbe:r");
+num1 = Number(window.prompt("I'll tell you the LARGER of two numbers! \n\nTell me your first number:"));
+num2 = Number(window.prompt("I'll tell you the LARGER of two numbers! \n\nTell me your second number:"));
 
-window.console.log(num1, num2);
+window.console.log(typeof num1, num1, typeof num2, num2);
 
-if (num1 > num1) {
+if (num1 > num2) {
     alert("I GOT IT!!\n\nThe larger number is: " + num1);
-} else {
+} else if (num1 < num2){
     alert("I GOT IT!!\n\nThe larger number is: " + num2);
+} else {
+    alert("Ummm... these numbers are the same.");
 }
 
