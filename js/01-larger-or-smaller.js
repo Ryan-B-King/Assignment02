96a3275b3ba6711cdc1ebb3c291293f10dfc847e
Rockharrd <33941531+Rockharrd@users.noreply.github.com>
2020-02-06 18:29:15 -0800

    Completed - Larger or Smaller

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,14 @@
+let num1;
+let num2;
+
+num1 = window.prompt("I'll tell you the LARGER of two numbers! \n\nTell me your first number:");
+num2 = window.prompt("I'll tell you the LARGER of two numbers! \n\nTell me your second numbe:r");
+
+window.console.log(num1, num2);
+
+if (num1 > num1) {
+    alert("I GOT IT!!\n\nThe larger number is: " + num1);
+} else {
+    alert("I GOT IT!!\n\nThe larger number is: " + num2);
+}
+
