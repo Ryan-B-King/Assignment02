97d269cdcaf7d1c1d6c52413be5e9727a6eb7d46
KING-PC\King <ryan13king@gmail.com>
2020-02-08 15:55:27 -0800

    Started Coinflip-redux

---
+++ js/03-coinflip-redux.js
@@ -0,0 +1,11 @@
+let coinFlip;
+let flip;
+
+for (flip=1; flip < 11; flip++) {
+    // window.console.log(flip);
+    result = Math.random(coinFlip);
+
+    window.console.log("Flip #: " + flip + " -- coinFlip result: " + result);
+}
+
+
