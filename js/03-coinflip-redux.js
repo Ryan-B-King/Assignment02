8caee188f2dd3dd9026e85f4a3370d512cd68c84
Rockharrd <33941531+Rockharrd@users.noreply.github.com>
2020-02-09 13:37:04 -0800

    Updated CFR

--- js/03-coinflip-redux.js
+++ js/03-coinflip-redux.js
@@ -8,5 +8,3 @@ for (flip=0; flip < 10; flip++) {
     final = (result === 0) ? "Heads" : "Tails";
     window.console.log("Flip #: " + flip + " -- coinFlip: " + result + " -- final: " + final);
 }
-
-
