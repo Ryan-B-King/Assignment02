1660e3ed600027ba4d11f7ae414430319fd19dd7
KING-PC\King <ryan13king@gmail.com>
2020-02-09 22:29:54 -0800

    Completed  Triangle

---
+++ js/05-triangle.js
@@ -0,0 +1,12 @@
+// GLOBAL VARIABLES
+let triangle = "#";
+let counter;
+
+// FOR LOOP TO CONCATENATE # AND MALKE TRIANGLE IN CONSOLE 
+for (counter = 1; counter < 7; counter++){
+    window.console.log(triangle);
+    triangle = triangle + "#";
+}
+window.console.log(triangle);
+
+
