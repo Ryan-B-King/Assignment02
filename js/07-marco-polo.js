e4af46c525a19b394dc33bc3bf34d3b647356c05
KING-PC\King <ryan13king@gmail.com>
2020-02-10 18:58:54 -0800

    Completed Marco Polo

--- js/07-marco-polo.js
+++ js/07-marco-polo.js
@@ -1,45 +1,15 @@
+// GLOBAL VARIABLES
 let i;
-let value;
-let marco;
-let polo;
-let marcoPolo;
 
-for (i = 1; i < 16; i++){
-    marco = (i % 3 === 0 && i % 5 !== 0) ? "Marco!" : i;
-    polo = (i % 3 !== 0 && i % 5 === 0) ? "Polo!" : i;
-    marcoPolo = (i % 3 === 0 && i % 5 === 0) ?  "Marco Polo!" : i;
-    // window.console.log(marco, polo, marcoPolo);
-
-    if (marco){
-        value = marco
-        window.console.log(value);
-    } else if (polo) {
-        value = polo;
-        window.console.log(value);
-    } else if (marcoPolo) {
-        value = marcoPolo;
-    }   else {
-        value = i;
+// FOR LOOP EVALUATING CONDITIONAL VALUES TO DETERMINE VARIOUS OUTPUT
+for (i = 1; i < 101; i++){
+    if (i % 3 === 0 && i % 5 === 0) {
+        window.console.log("Marco! Polo!");
+    } else if (i % 5 === 0) {
+        window.console.log("Polo!");
+    } else if (i % 3 === 0){
+        window.console.log("Marco!");
+    } else {
+        window.console.log(i);
     }
-    window.console.log(value);
-
-
-
-
-    // marco = (i % 3 === 0 && i % 5 !== 0) ? "Marco!" : i;
-    // // window.console.log(value);
-    // polo = (i % 3 !== 0 && i % 5 === 0) ? "Polo!" : i;
-    // // window.console.log(value);
-    // marcoPolo = (i % 3 === 0 && i % 5 === 0) ?  "Marco Polo!" : i;
-    // window.console.log(marco, polo, marcoPolo);
-
-    // switch (value){
-    //     case "Marco Polo!" : window.console.log("Marco Polo!");
-    //         break;
-    //     case "Polo!" : window.console.log("Polo!");
-    //         break;
-    //     case "Marco!" : window.console.log("Marco!");
-    //         break;
-    //     default : window.console.log(value);
-    // }
 }
